0c898b360cebf8daec829cae2a81f325be7d52bc
Youhei SASAKI <uwabami@gfd-dennou.org>
2016-12-20 18:35:39 +0900

    Add el headers
    Signed-off-by: Youhei SASAKI <uwabami@gfd-dennou.org>

--- eaw_and_emoji.el
+++ eaw_and_emoji.el
@@ -1,3 +1,29 @@
+;;; locale-eaw-emoji.el -- set UAX11 and Emoji as double width
+
+;; Author: Youhei SASAKI <uwabami@gfd-dennou.org>
+;; Update: 2016-12-20 18:34:23 +0900
+;;; License
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+;; .
+;; The above copyright notice and this permission notice shall be included
+;; in all copies or substantial portions of the Software.
+;; .
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+;; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentry
+;;; Code
 (setq east-asian-ambiguous-and-emoji
       '(
         #x00A1 ; INVERTED EXCLAMATION MARK
@@ -1867,6 +1893,7 @@
         #x1F984 ; UNICORN FACE
         #x1F9C0 ; CHEESE WEDGE
         ))
+;;;###autoload
 (defun eaw-and-emoji-set-width (width)
   (while (char-table-parent char-width-table)
     (setq char-width-table (char-table-parent char-width-table)))
@@ -1877,6 +1904,7 @@
     (set-char-table-parent table char-width-table)
     (setq char-width-table table)))
 
+;;;###autoload
 (defun eaw-and-emoji-fullwidth ()
   (setq nobreak-char-display nil)
   (eaw-and-emoji-set-width 2))
--- generate.rb
+++ generate.rb
@@ -190,6 +190,34 @@ EOS
 }
 
 File.open(eaw_and_emoji_elisp, 'w+'){|f|
+  f.puts <<-EOS
+;;; locale-eaw-emoji.el -- set UAX11 and Emoji as double width
+
+;; Author: Youhei SASAKI <uwabami@gfd-dennou.org>
+;; Update: #{Time.now}
+;;; License
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+;; .
+;; The above copyright notice and this permission notice shall be included
+;; in all copies or substantial portions of the Software.
+;; .
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+;; OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+;;; Commentry
+;;; Code
+EOS
   f.puts "(setq east-asian-ambiguous-and-emoji\n      '("
   list_eaw.each {|k, v|
     f.puts sprintf("        #x%s ; %s", k, v)
@@ -199,6 +227,7 @@ File.open(eaw_and_emoji_elisp, 'w+'){|f|
   }
   f.puts '        ))'
   f.puts <<-EOS
+;;;###autoload
 (defun eaw-and-emoji-set-width (width)
   (while (char-table-parent char-width-table)
     (setq char-width-table (char-table-parent char-width-table)))
@@ -209,6 +238,7 @@ File.open(eaw_and_emoji_elisp, 'w+'){|f|
     (set-char-table-parent table char-width-table)
     (setq char-width-table table)))
 
+;;;###autoload
 (defun eaw-and-emoji-fullwidth ()
   (setq nobreak-char-display nil)
   (eaw-and-emoji-set-width 2))
